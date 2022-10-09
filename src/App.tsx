55e511fdb277b4b58b11ec1334599064615467ff
Fabio Leardini <leardinifabio90@gmail.com>
2022-10-09 01:49:59 -0300

    criacao header e input tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./global.css";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <header>
+        <div className="logoToDo">
+          <img src="../public/rocket.svg" />
+          <h1>
+            to<span>do</span>
+          </h1>
+        </div>
+      </header>
+      <label className="inputNewTask">
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button type="submit">
+          Criar <span>+</span>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </label>
     </div>
-  )
+  );
 }
-
-export default App
