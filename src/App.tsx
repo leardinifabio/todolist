97dbc67abd1bc07e8219a50bafa40f173a30994a
Fabio Leardini <leardinifabio90@gmail.com>
2022-10-10 23:48:14 -0300

    criacao do componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
+import { Header } from "./components/Header";
 import "./global.css";
 import "./App.css";
 
 export function App() {
-  return (
-    <div>
-      <header>
-        <div className="logoToDo">
-          <img src="../public/rocket.svg" />
-          <h1>
-            to<span>do</span>
-          </h1>
-        </div>
-      </header>
-      <label className="inputNewTask">
-        <input type="text" placeholder="Adicione uma nova tarefa" />
-        <button type="submit">
-          Criar <span>+</span>
-        </button>
-      </label>
-    </div>
-  );
+  return <Header />;
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import style from "./Header.module.css";
+
+export function Header() {
+  return (
+    <div>
+      <header>
+        <div className={style.logoToDo}>
+          <img src="../public/rocket.svg" />
+          <h1>
+            to<span>do</span>
+          </h1>
+        </div>
+      </header>
+      <label className={style.inputNewTask}>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button type="submit">
+          Criar <span>+</span>
+        </button>
+      </label>
+    </div>
+  );
+}
