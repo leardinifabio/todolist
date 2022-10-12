292a28124b3fdb0da830fab41446555f1f7bda65
Fabio Leardini <leardinifabio90@gmail.com>
2022-10-12 16:03:04 -0300

    estilo componente task e funcionalidade

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,12 +4,12 @@ import { Circle, Trash } from "phosphor-react";
 export function Task() {
   return (
     <div className={style.taskBox}>
-      <Circle size="1.5rem" color="#4ea8de" />
+      <Circle className={style.circleButton} size="1.5rem" />
       <span>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
         suscipit iste blanditiis autem?
       </span>
-      <Trash size="1.4rem" color="#808080" />
+      <Trash className={style.trashButton} size="1.4rem" />
     </div>
   );
 }
--- src/components/TaskContainer.tsx
+++ src/components/TaskContainer.tsx
@@ -38,7 +38,9 @@ export function TaskContainer() {
             <span>Crie tarefas e organize seus itens a fazer</span>
           </div>
         ) : (
-          <Task />
+          tasks.map((task) => {
+            return <Task />;
+          })
         )}
       </main>
     </div>
