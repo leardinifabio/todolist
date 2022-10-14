08377891857a83432267b09821632d5e8cb90625
Fabio Leardini <leardinifabio90@gmail.com>
2022-10-13 22:18:40 -0300

    add opção de excluir e concluir tarefa

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,19 +4,50 @@ import { Circle, CheckCircle, Trash } from "phosphor-react";
 interface Task {
   id: number;
   content: string;
-  isNotCompleted?: boolean;
+  isNotCompleted: boolean;
 }
 
-export function Task({ content, isNotCompleted }: Task) {
+interface TaskProps {
+  id: Task["id"];
+  content: Task["content"];
+  isNotCompleted: Task["isNotCompleted"];
+  onDeleteComment: (taskToDelete: number) => void;
+  onFinishTask: (
+    taskToFinishId: number,
+    taskToFinishIsNotCompleted: boolean
+  ) => void;
+}
+
+export function Task({
+  id,
+  content,
+  isNotCompleted,
+  onDeleteComment,
+  onFinishTask,
+}: TaskProps) {
+  function handleDeleteComment() {
+    onDeleteComment(id);
+    console.log(id);
+  }
+
+  function handleFinishTask() {
+    onFinishTask(id, isNotCompleted);
+  }
+
   return (
     <div className={style.taskBox}>
       {isNotCompleted ? (
-        <Circle className={style.circleButton} size="1.5rem" />
+        <Circle
+          className={style.circleButton}
+          size="1.5rem"
+          onClick={handleFinishTask}
+        />
       ) : (
         <CheckCircle
           className={style.circleButton}
           size="1.5rem"
           weight="fill"
+          onClick={handleFinishTask}
         />
       )}
 
@@ -27,7 +58,11 @@ export function Task({ content, isNotCompleted }: Task) {
       >
         {content}
       </span>
-      <Trash className={style.trashButton} size="1.4rem" />
+      <Trash
+        className={style.trashButton}
+        size="1.4rem"
+        onClick={handleDeleteComment}
+      />
     </div>
   );
 }
--- src/components/TaskContainer.tsx
+++ src/components/TaskContainer.tsx
@@ -1,29 +1,46 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { ClipboardText } from "phosphor-react";
 import { Task } from "./Task";
 import style from "./TaskContainer.module.css";
 
+interface Task {
+  id: number;
+  content: string;
+  isNotCompleted: boolean;
+}
+
 export function TaskContainer() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      isNotCompleted: true,
-    },
-    {
-      id: 2,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      isNotCompleted: false,
-    },
-    {
-      id: 3,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      isNotCompleted: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleNewTask(event: FormEvent) {
+    event.preventDefault();
+    setTasks([
+      ...tasks,
+      { id: tasks.length + 1, content: newTask, isNotCompleted: true },
+    ]);
+    setNewTask("");
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function deleteComent(taskToDelete: number) {
+    const taskWithoutDelete = tasks.filter((task) => task.id !== taskToDelete);
+    setTasks(taskWithoutDelete);
+  }
+
+  function finishTask(
+    taskToFinishId: number,
+    taskFinishIsNotCompleted: boolean
+  ) {
+    const taskToFinish = tasks.find((task) => task.id === taskToFinishId);
+    if (taskToFinish) {
+      taskToFinish.isNotCompleted = !taskFinishIsNotCompleted;
+      setTasks([...tasks]);
+    }
+  }
 
   const isCompleted = tasks.filter(
     (task) => task.isNotCompleted === false
@@ -32,8 +49,13 @@ export function TaskContainer() {
   return (
     <div className={style.container}>
       <header>
-        <form className={style.inputNewTask}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
+        <form onSubmit={handleNewTask} className={style.inputNewTask}>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            value={newTask}
+            onChange={handleNewTaskChange}
+          />
           <button type="submit">
             Criar <span>+</span>
           </button>
@@ -64,6 +86,8 @@ export function TaskContainer() {
                 id={task.id}
                 content={task.content}
                 isNotCompleted={task.isNotCompleted}
+                onDeleteComment={deleteComent}
+                onFinishTask={finishTask}
               />
             );
           })
